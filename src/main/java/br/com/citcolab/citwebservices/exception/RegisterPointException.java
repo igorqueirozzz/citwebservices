115c139bfa365280d1e42b81fb6eaef805ba81a2
Igor Queiroz <igorqueirozz@outlook.com>
2021-03-26 17:55:42 -0300

    #Feito alguns ajustes

---
+++ src/main/java/br/com/citcolab/citwebservices/exception/RegisterPointException.java
@@ -0,0 +1,36 @@
+package br.com.citcolab.citwebservices.exception;
+
+import br.com.citcolab.citwebservices.model.entity.PointRegister;
+
+import java.util.Calendar;
+import java.util.List;
+
+public class RegisterPointException extends RuntimeException {
+
+    private static final long serialVersionUID = 3054527412432131539L;
+
+    public RegisterPointException(String message){
+        super(message);
+    }
+
+    public RegisterPointException(){}
+
+    public static void checkDateTime(Calendar date){
+        if(date.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY || date.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY){
+            throw new RegisterPointException("Registro não autorizado fora da jornada de trabalho");
+        }
+    }
+
+    public static void checkRegisterTime(PointRegister pointRegister, List<PointRegister> pointRegisterList, Integer amount){
+
+        if (pointRegister == null){
+            throw new RegisterPointException("Registro de ponto não encontrado");
+        }
+
+        if (amount == 4){
+            throw new RegisterPointException("Quantidade de registros diários atinginda");
+        }
+
+    }
+
+}
